fb5327105c96673ee102a8de3dd29bbc19ef2c17
regina <usmanova.re@yandex.ru>
2024-03-15 23:18:55 +0500

    Decor

---
+++ app/src/main/java/com/example/weathertrackr/WeatherTrackr.kt
@@ -0,0 +1,15 @@
+package com.example.weathertrackr
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class WeatherTrackr : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_weather_trackr)
+    }
+}
